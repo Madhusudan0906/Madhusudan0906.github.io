2d2eed53cd3800e855e462d6234a3599b5a077a4
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-11-05 21:01:19 +0530

    added contactspage

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,29 @@
 import { Heading } from "@chakra-ui/react";
 import {AiFillGithub,AiFillPhone} from "react-icons/ai";
+import {GrLinkedinOption} from "react-icons/gr";
 import styles from "./Contact.module.css"
 export default function Contact(){
     return (
         <div className={styles.main}>
             <Heading>Contact Me</Heading>
-        <div>
+            <div className={styles.contact}>
+            <div className={styles.cicon}>
             <a href="https://github.com/Madhusudan0906" target="_blank">
-        <AiFillGithub size="3em"/></a>
-        <a href="tel:9893560297" target="_blank"><AiFillPhone size="3em"/></a>
-        <a href="" target="_blank"></a>
+        <AiFillGithub size="4em"/></a>
+        <a href="tel:9893560297" target="_blank"><AiFillPhone size="4em"/></a>
+        <a href="https://www.linkedin.com/in/madhusudan-prajapat-918808169/" target="_blank"><GrLinkedinOption size="4em" /></a>
         </div>
+        <div>
+            <form className={styles.cform}>
+                <br/>
+                <input type="text" placeholder="Name" /><br/>
+                <input type="text" placeholder="Email"/><br/>
+                <textarea placeholder="Query"></textarea><br/>
+                <input type="submit" style={{width:"100px",backgroundColor:"darkblue",fontSize:"18px",border:"none",borderRadius:"7px",padding:"5px 10px"}}/><br/>
+            </form>
+        </div>
+            </div>
+        
         </div>
     );
 }
